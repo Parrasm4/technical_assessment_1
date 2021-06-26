36119e033adf08426546a9c9a573f0a99a41ca3a
Sumit Lal <sumitlal.techdev@gmail.com>
2021-06-26 09:20:48 -0700

    Adding more clarity to tests

--- src/meal.js
+++ src/meal.js
@@ -1,10 +1,15 @@
 /* 1
 ----Write a function called nameMenuItem that takes in any ONE item
 ----and returns a string with the word "Delicious" in front of the item name
+----Example: nameMenuItem("Pizza") -> "Delicous Pizza"
+----Example: nameMenuItem("Tacos") -> "Delicous Tacos"
 */
 
+function nameMenuItem(item) {
+    // Please write your code here
+}
 
-// Please write your code here
+
 
 
 
@@ -13,9 +18,11 @@
 /* 2
 ----Write a function called createMeuItem that creates and return an object with the new item name, price and a type
 ----Your function should return an object with the keys of 'name', 'price' and 'type'
+----Example: createMenuItem("Cheesecake", 7.50, "Pastry") : createMenuItem.name -> "Cheesecake", createMenuItem.price -> 7.50, createMenuItem.type -> "Pastry"
 */
-
-// Please write your code here
+function createMenuItem(name, price, type){
+    // Please write your code here
+}
 
 
 
@@ -23,10 +30,14 @@
 
 /* 3
 ----Write a function called addIngridients that takes in the name of
-----the ingredient and an array to store it in.
+----the ingredient and an array. Your function should add the ingredient to the ingriedientArray if it does not already exist.
 ----Please be sure to not let the same ingredient be added on twice.
+----addIngredients("Flour", ["Water", "Butter"]) -> ["Water", "Butter", "Flour"];
+----addIngredients("Water", ["Water", "Butter"]) -> ["Water", "Butter"];
 */
-// Please write your code here
+function addIngredients(ingredient, ingredientArray) {
+    // Please write your code here
+}
 
 
 
@@ -34,39 +45,41 @@
 /* 4
 ----Write a function called formatPrice that takes in a price
 ----and returns a '$' in front of the price passed in
+----formatPrice(23.12) -> $23.12;
 */
+function formatPrice(price){
+    // Please write your code here
+}
 
-// Please write your code here
+
+
+
 
 
 
 
 
 /* 5
-----Write a function called formatPrice that takes in a price
-----and returns a '$' in front of the price passed in
+----Write a function called decreasePrice that takes in a price
+----and returns a price with a 10% reduction
+----descreasePrice(100.00) -> 90.00
 */
+function decreasePrice(price){
+    // Please write your code here
+}
 
-// Please write your code here
+
 
 
 
 
 
 /* 6
-----Write a function called decreasePrice that takes in a price
-----and returns a price with a 10% reduction
-*/
-
-// Please write your code here
-
-
-
-
-/* 7
 ----Write a function called createRecipe that takes in a recipeTitle, recipeIngredients, and a menuType
 ----your function should create and return a recipe object with the following keys
 ----'title', 'ingredients', and 'type'
+----createRecipe("Delicious Grilled Cheese", ["bread", "cheese", "butter"], "lunch");  recipe.title -> "Delicious Grilled Cheese"; recipe.recipeIngredients -> ["bread", "cheese", "butter"]; recipe.menuType -> "lunch";
 */
-
-// Please write your code here
+function createRecipe(recipeTitle, recipeIngredients, menuType) {
+    // Please write your code here
+}
--- src/order.js
+++ src/order.js
@@ -1,53 +1,13 @@
 
-/* 3
-----Write a function called addIngridients that takes in the name of
-----the ingredient and an array to store it in.
-----Please be sure to not let the same ingredient be added on twice.
-*/
-// Please write your code here
-
-
-
-
-/* 4
-----Write a function called formatPrice that takes in a price
-----and returns a '$' in front of the price passed in
-*/
-
-// Please write your code here
-
-
-
-
-
-/* 5
-----Write a function called formatPrice that takes in a price
-----and returns a '$' in front of the price passed in
-*/
-
-// Please write your code here
-
-
-
-
-
-/* 6
-----Write a function called decreasePrice that takes in a price
-----and returns a price with a 10% reduction
-*/
-
-// Please write your code here
-
-
-
 
 /* 1
 ----Write a function called takeOrder that takes in a order and an orderArray
-----your function should add to the ordersArray
+----your function should add the order to the ordersArray
 ----ordersArray should not be able to hold more than three orders at a time
 */
-
-// Please write your code here
+function takeOrder(order, orderArray) {
+    // Please write your code here
+}
 
 
 
@@ -57,8 +17,12 @@
 ----Write a function called refundOrder that takes in a orderNumber and an array of orders
 ----Your function should remove the order from the array of orders simply by the orderNumber
 */
+function refundOrder(orderNumber, orderArray) {
+    // Please write your code here
+}
 
-// Please write your code here
+
+
 
 
 
@@ -67,11 +31,15 @@
 
 
 /* 3
-----Write a function called addIngridients that takes in the name of
-----the ingredient and an array to store it in.
-----Please be sure to not let the same ingredient be added on twice.
+----Write a function called listItems that takes in array of orders and
+----lists out all of the order items by name
+----HINT: please console log the array that is being passed into listItems
 */
-// Please write your code here
+function listItems(arr){
+    // Please write your code here
+}
+
+
 
 
 
@@ -80,24 +48,9 @@
 
 
 /* 4
-----Write a function called listItems that takes in array of orders and
-----lists out all of the order items by name
-----HINT: please console log the array that is being passed into listItems
-----I Have started out this code for you, just uncomment and continue working
-*/
-
-
-// Please write your code here
-
-// function listItems(arr){
-// }
-
-
-
-
-
-
-/* 5
 ----Write a function called searchOrder that takes in array of orders and
-----a item. Your function should output if the item is included in the array of orders or not
+----a item. Your function should output true if the item is included in the array of orders and false if otherwise
 */
+function searchOrder(orderArray, item) {
+    // Please write your code here
+}
